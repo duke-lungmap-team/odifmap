1db69ccc93f83b2ff82feea526cafc984d1da0b9
whitews <whitews@gmail.com>
2020-12-04 14:58:24 -0500

    update dependencies in requirements.txt & setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,19 @@ setup(
     license='BSD 2-Clause License',
     long_description=open('README.md').read(),
     author='Scott White',
-    description='A Python library for object segmentation & classification in immunofluorescence microscopy images',
+    description='A Python library for ontology-driven object segmentation & classification '
+                'in immunofluorescence microscopy images',
     install_requires=[
-        'scikit-learn (>=0.20.3)',
-        'scikit-image (>=0.15)',
-        'opencv-python (>=4.1)',
-        'scipy (>=1.2)',
-        'numpy (>=1.16)',
-        'matplotlib (>=3.0)',
-        'Pillow (>=6.2.2)',
-        'pandas (>=0.24)',
-        'xgboost (>=1.1)',
-        'cv2-extras @ git+https://github.com/whitews/cv2-extras.git'
+        'scikit-learn (>=0.23.1)',
+        'scikit-image (>=0.17.2)',
+        'opencv-python (>=4.3.0.36)',
+        'scipy (>=1.5.3)',
+        'numpy (>=1.19)',
+        'matplotlib (>=3.3)',
+        'Pillow (>=8.0.1)',
+        'pandas (>=1.0.5)',
+        'xgboost (>=1.1.1)',
+        'cv2-extras @ git+https://github.com/whitews/cv2-extras.git',
+        'ontospy (>=1.9.8.3)'
     ]
 )
