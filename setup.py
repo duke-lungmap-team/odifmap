a63aab533c8061e3449f17c80870fff26e9ca268
whitews <whitews@gmail.com>
2020-12-06 23:48:14 -0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='odifmap',
-    version='0.3.0b',
+    version='0.3.0',
     packages=['odifmap'],
     license='BSD 2-Clause License',
     long_description=open('README.md').read(),
